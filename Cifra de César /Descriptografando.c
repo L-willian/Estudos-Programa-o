6a95c447d423d976e14d176c9232d5a86a3a7230
Luiz Willian <luizwilliasoares930@gmail.com>
2026-06-11 10:23:44 -0300

    Adicionar implementação da cifra de César para criptografia e descriptografia de texto

--- Cifra de César /Descriptografando.c
+++ Cifra de César /Descriptografando.c
@@ -4,25 +4,26 @@
 
 int main()
 {
-    // Solicita ao usuário o texto a ser criptografado
+
+    // Solicita ao usuario o texto a ser descriptografado
     char texto[100];
     int chave = 3;
 
     // Le o texto do usuario
-    printf("Digite o texto a ser criptografado: ");
+    printf("Digite o texto a ser descriptografado: ");
     fgets(texto, sizeof(texto), stdin);
 
     // Remove o caractere de nova linha, se presente
     texto[strcspn(texto, "\n")] = '\0';
 
-    // Criptografa o texto usando a cifra de Cesar
+    // Descriptografa o texto usando a cifra de Cesar
     for (int i = 0; texto[i] != '\0'; i++)
     {
-        texto[i] = texto[i] + chave;
+        texto[i] = texto[i] - chave;
     }
 
-    // Exibe o texto criptografado
-    printf("Texto criptografado: %s\n", texto);
+    // Exibe o texto descriptografado
+    printf("Texto descriptografado: %s\n", texto);
 
     return 0;
 }
